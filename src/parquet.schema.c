8620f7a79fbdcbb5b163019225b21f15d2700acc
Adrian Macal <amacal@id5.io>
2025-10-03 17:39:50 +0000

    Data03 tests.

--- src/parquet.schema.c
+++ src/parquet.schema.c
@@ -122,14 +122,14 @@ static void can_open_schema_data01() {
   assert(schema.type_length == PARQUET_UNKNOWN_VALUE, "should have unknown type length");
 
   // assert the first child
-  assert_eq_str(schema.children.elements[0][0].name, "date", "should have a name for the first child");
-  assert(schema.children.elements[0][0].children.count == 0, "should have no children");
-  assert(schema.children.elements[0][0].children.elements == NULL, "should have no children elements");
+  assert_eq_str(schema.children.elements[0]->name, "date", "should have a name for the first child");
+  assert(schema.children.elements[0]->children.count == 0, "should have no children");
+  assert(schema.children.elements[0]->children.elements == NULL, "should have no children elements");
 
-  assert(schema.children.elements[0][0].repeated_type == PARQUET_REPETITION_TYPE_OPTIONAL, "should be optional");
-  assert(schema.children.elements[0][0].converted_type == PARQUET_CONVERTED_TYPE_DATE, "should be date");
-  assert(schema.children.elements[0][0].data_type == PARQUET_DATA_TYPE_INT32, "should have int32 data type");
-  assert(schema.children.elements[0][0].type_length == PARQUET_UNKNOWN_VALUE, "should have unknown type length");
+  assert(schema.children.elements[0]->repeated_type == PARQUET_REPETITION_TYPE_OPTIONAL, "should be optional");
+  assert(schema.children.elements[0]->converted_type == PARQUET_CONVERTED_TYPE_DATE, "should be date");
+  assert(schema.children.elements[0]->data_type == PARQUET_DATA_TYPE_INT32, "should have int32 data type");
+  assert(schema.children.elements[0]->type_length == PARQUET_UNKNOWN_VALUE, "should have unknown type length");
 
   // assert the second child
   assert_eq_str(schema.children.elements[0][1].name, "hour", "should have a name for the second child");
@@ -216,24 +216,115 @@ static void can_open_schema_data02() {
   assert(schema.type_length == PARQUET_UNKNOWN_VALUE, "should have unknown type length");
 
   // assert the 7th child
-  assert_eq_str(schema.children.elements[6][0].name, "id5uidchanged", "should have a name for the 7th child");
-  assert(schema.children.elements[6][0].children.count == 0, "should have no children");
-  assert(schema.children.elements[6][0].children.elements == NULL, "should have no children elements");
+  assert_eq_str(schema.children.elements[6]->name, "id5uidchanged", "should have a name for the 7th child");
+  assert(schema.children.elements[6]->children.count == 0, "should have no children");
+  assert(schema.children.elements[6]->children.elements == NULL, "should have no children elements");
 
-  assert(schema.children.elements[6][0].repeated_type == PARQUET_REPETITION_TYPE_OPTIONAL, "should be optional");
-  assert(schema.children.elements[6][0].converted_type == PARQUET_CONVERTED_TYPE_NONE, "should be missing");
-  assert(schema.children.elements[6][0].data_type == PARQUET_DATA_TYPE_BOOLEAN, "should have boolean data type");
-  assert(schema.children.elements[6][0].type_length == PARQUET_UNKNOWN_VALUE, "should have unknown type length");
+  assert(schema.children.elements[6]->repeated_type == PARQUET_REPETITION_TYPE_OPTIONAL, "should be optional");
+  assert(schema.children.elements[6]->converted_type == PARQUET_CONVERTED_TYPE_NONE, "should be missing");
+  assert(schema.children.elements[6]->data_type == PARQUET_DATA_TYPE_BOOLEAN, "should have boolean data type");
+  assert(schema.children.elements[6]->type_length == PARQUET_UNKNOWN_VALUE, "should have unknown type length");
 
   // assert the 113th child
-  assert_eq_str(schema.children.elements[112][0].name, "cookie_prediction_probability", "should be cookie...");
-  assert(schema.children.elements[112][0].children.count == 0, "should have no children");
-  assert(schema.children.elements[112][0].children.elements == NULL, "should have no children elements");
+  assert_eq_str(schema.children.elements[112]->name, "cookie_prediction_probability", "should be cookie...");
+  assert(schema.children.elements[112]->children.count == 0, "should have no children");
+  assert(schema.children.elements[112]->children.elements == NULL, "should have no children elements");
 
-  assert(schema.children.elements[112][0].repeated_type == PARQUET_REPETITION_TYPE_OPTIONAL, "should be optional");
-  assert(schema.children.elements[112][0].converted_type == PARQUET_CONVERTED_TYPE_NONE, "should be missing");
-  assert(schema.children.elements[112][0].data_type == PARQUET_DATA_TYPE_FLOAT, "should have float data type");
-  assert(schema.children.elements[112][0].type_length == PARQUET_UNKNOWN_VALUE, "should have unknown type length");
+  assert(schema.children.elements[112]->repeated_type == PARQUET_REPETITION_TYPE_OPTIONAL, "should be optional");
+  assert(schema.children.elements[112]->converted_type == PARQUET_CONVERTED_TYPE_NONE, "should be missing");
+  assert(schema.children.elements[112]->data_type == PARQUET_DATA_TYPE_FLOAT, "should have float data type");
+  assert(schema.children.elements[112]->type_length == PARQUET_UNKNOWN_VALUE, "should have unknown type length");
+
+  // close the parquet file
+  parquet_close(&file);
+
+  // destroy the pool
+  malloc_destroy(&pool);
+}
+
+static void can_open_schema_data03() {
+  i64 result;
+
+  struct parquet_file file;
+  struct malloc_pool pool;
+
+  struct parquet_metadata metadata;
+  struct parquet_schema schema;
+  struct parquet_schema *child;
+
+  // initialize the pool
+  malloc_init(&pool);
+
+  // initialize the parquet file
+  parquet_init(&file, &pool);
+
+  // open a valid parquet file
+  result = parquet_open(&file, "data/test03.parquet");
+  assert(result == 0, "should open parquet file");
+
+  // parse the metadata
+  result = parquet_parse(&file, &metadata);
+  assert(result == 0, "should parse metadata");
+
+  // and open the schema
+  result = parquet_open_schema(&file, &metadata, &schema);
+  assert(result == 0, "should open schema");
+
+  // assert the schema values
+  assert_eq_str(schema.name, "trino_schema", "should have correct schema name");
+  assert(schema.children.count == 13, "should have 13 children");
+  assert(schema.children.elements != NULL, "should have children elements");
+
+  assert(schema.repeated_type == PARQUET_REPETITION_TYPE_NONE, "should have missing repeated type");
+  assert(schema.converted_type == PARQUET_CONVERTED_TYPE_NONE, "should be missing");
+  assert(schema.data_type == PARQUET_DATA_TYPE_NONE, "should have missing data type");
+  assert(schema.type_length == PARQUET_UNKNOWN_VALUE, "should have unknown type length");
+
+  // assert the 8th child
+  assert_eq_str(schema.children.elements[7]->name, "bidderrequests", "should have a name for the 8th child");
+  assert(schema.children.elements[7]->children.count == 1, "should have 1 child");
+  assert(schema.children.elements[7]->children.elements != NULL, "should have children elements");
+
+  assert(schema.children.elements[7]->repeated_type == PARQUET_REPETITION_TYPE_OPTIONAL, "should be optional");
+  assert(schema.children.elements[7]->converted_type == PARQUET_CONVERTED_TYPE_LIST, "should be list");
+  assert(schema.children.elements[7]->data_type == PARQUET_DATA_TYPE_NONE, "should be missing data type");
+  assert(schema.children.elements[7]->type_length == PARQUET_UNKNOWN_VALUE, "should have unknown type length");
+
+  // assert the 8.1th child
+  child = schema.children.elements[7];
+
+  assert_eq_str(child->children.elements[0]->name, "list", "should be named 'list'");
+  assert(child->children.elements[0]->children.count == 1, "should have 1 child");
+  assert(child->children.elements[0]->children.elements != NULL, "should have children elements");
+
+  assert(child->children.elements[0]->repeated_type == PARQUET_REPETITION_TYPE_REPEATED, "should be repeated");
+  assert(child->children.elements[0]->converted_type == PARQUET_CONVERTED_TYPE_NONE, "should be missing");
+  assert(child->children.elements[0]->data_type == PARQUET_DATA_TYPE_NONE, "should be missing data type");
+  assert(child->children.elements[0]->type_length == PARQUET_UNKNOWN_VALUE, "should have unknown type length");
+
+  // assert the 8.1.1th child
+  child = child->children.elements[0];
+
+  assert_eq_str(child->children.elements[0]->name, "element", "should be named 'element'");
+  assert(child->children.elements[0]->children.count == 7, "should have 7 children");
+  assert(child->children.elements[0]->children.elements != NULL, "should have children elements");
+
+  assert(child->children.elements[0]->repeated_type == PARQUET_REPETITION_TYPE_OPTIONAL, "should be optional");
+  assert(child->children.elements[0]->converted_type == PARQUET_CONVERTED_TYPE_NONE, "should be missing");
+  assert(child->children.elements[0]->data_type == PARQUET_DATA_TYPE_NONE, "should be missing data type");
+  assert(child->children.elements[0]->type_length == PARQUET_UNKNOWN_VALUE, "should have unknown type length");
+
+  // assert the 8.1.1.1th child
+  child = child->children.elements[0];
+
+  assert_eq_str(child->children.elements[0]->name, "adunitcode", "should be named 'adunitcode'");
+  assert(child->children.elements[0]->children.count == 0, "should have 0 children");
+  assert(child->children.elements[0]->children.elements == NULL, "should have no children elements");
+
+  assert(child->children.elements[0]->repeated_type == PARQUET_REPETITION_TYPE_OPTIONAL, "should be optional");
+  assert(child->children.elements[0]->converted_type == PARQUET_CONVERTED_TYPE_UTF8, "should be UTF8");
+  assert(child->children.elements[0]->data_type == PARQUET_DATA_TYPE_BYTE_ARRAY, "should be byte array data type");
+  assert(child->children.elements[0]->type_length == PARQUET_UNKNOWN_VALUE, "should have unknown type length");
 
   // close the parquet file
   parquet_close(&file);
@@ -245,6 +336,7 @@ static void can_open_schema_data02() {
 void parquet_test_cases_schema(struct runner_context *ctx) {
   test_case(ctx, "can open schema data01", can_open_schema_data01);
   test_case(ctx, "can open schema data02", can_open_schema_data02);
+  test_case(ctx, "can open schema data03", can_open_schema_data03);
 }
 
 #endif
